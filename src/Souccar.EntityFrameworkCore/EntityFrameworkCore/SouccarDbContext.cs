e0ba5a146cd971439d9591272d69ed938cc8e70b
abdk123 <abdok1234@gmail.com>
2024-07-13 11:24:26 +0300

    5156

--- src/Souccar.EntityFrameworkCore/EntityFrameworkCore/SouccarDbContext.cs
+++ src/Souccar.EntityFrameworkCore/EntityFrameworkCore/SouccarDbContext.cs
@@ -11,6 +11,9 @@ using Souccar.SaleManagement.Settings.Materials;
 using Souccar.SaleManagement.Settings.Stores;
 using Souccar.SaleManagement.Settings.Units;
 using Souccar.SaleManagement.Stocks;
+using Souccar.SaleManagement.PurchaseInvoises;
+using Souccar.SaleManagement.Deliveries;
+using Souccar.SaleManagement.PurchaseOrders.Receives;
 
 namespace Souccar.EntityFrameworkCore
 {
@@ -24,12 +27,20 @@ namespace Souccar.EntityFrameworkCore
         public DbSet<Store> Store { get; set; }
         public DbSet<Material> Material { get; set; }
         public DbSet<Customer> Customer { get; set; }
+        public DbSet<CustomerBalance> CustomerBalance { get; set; }
         public DbSet<ClearanceCompany> ClearanceCompany { get; set; }
         public DbSet<TransportCompany> TransportCompany { get; set; }
         public DbSet<Category> Category { get; set; }
-        public DbSet<Offer> Offer { get; set; }
         public DbSet<ClearanceCompanyBalance> ClearanceCompanyBalance { get; set; }
         public DbSet<TransportCompanyBalance> TransportCompanyBalance { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+        public DbSet<InvoiceItem> InvoiceItem { get; set; }
+        public DbSet<Offer> Offer { get; set; }
+        public DbSet<OfferItem> OfferItem { get; set; }
+        public DbSet<Delivery> Delivery { get; set; }
+        public DbSet<DeliveryItem> DeliveryItem { get; set; }
+        public DbSet<Receiving> Receiving { get; set; }
+        public DbSet<ReceivingItem> ReceivingItem { get; set; }
 
         public SouccarDbContext(DbContextOptions<SouccarDbContext> options)
             : base(options)
