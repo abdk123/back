b8c6ecc1e78751155f0e46bee4c3a268fdd31d98
MohamadTonbary <ttmohamad299@gmail.com>
2024-08-01 20:37:56 +0300

    edit receive

--- src/Souccar.Application/SaleManagement/PurchaseOrders/Receives/Services/ReceivingAppService.cs
+++ src/Souccar.Application/SaleManagement/PurchaseOrders/Receives/Services/ReceivingAppService.cs
@@ -4,6 +4,9 @@ using Souccar.Core.Services;
 using Souccar.SaleManagement.Receivings.Services;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Souccar.SaleManagement.PurchaseOrders.Offers.Dto;
+using Souccar.SaleManagement.PurchaseOrders.Offers;
 
 namespace Souccar.SaleManagement.PurchaseOrders.Receives.Services
 {
@@ -20,6 +23,22 @@ namespace Souccar.SaleManagement.PurchaseOrders.Receives.Services
         {
             var receiving = _receivingDomainService.GetAllByInvoiceId(invoiceId).ToList();
             return ObjectMapper.Map<List<ReceivingDto>>(receiving);
+        }
+
+        public override async Task<ReceivingDto> UpdateAsync(UpdateReceivingDto input)
+        {
+            var oldOffer = await _receivingDomainService.GetAsync(input.Id);
+            ObjectMapper.Map<UpdateReceivingDto, Receiving>(input, oldOffer);
+            var newOffer = await _receivingDomainService.UpdateAsync(oldOffer);
+            var items = await Task.FromResult(_receivingDomainService.GetItemsByReceivingId(oldOffer.Id));
+            foreach (var item in items)
+            {
+                if (!input.ReceivingItems.Any(x => x.Id == item.Id))
+                {
+                    await _receivingDomainService.DeleteItemAsync(item.Id);
+                }
+            }
+            return ObjectMapper.Map<ReceivingDto>(newOffer);
         }
     }
 }
--- src/Souccar.Core/SaleManagement/PurchaseOrders/Receives/Services/ReceivingDomainService.cs
+++ src/Souccar.Core/SaleManagement/PurchaseOrders/Receives/Services/ReceivingDomainService.cs
@@ -1,17 +1,22 @@
 using Abp.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Souccar.Core.Services.Implements;
+using Souccar.SaleManagement.PurchaseOrders.Offers;
 using Souccar.SaleManagement.PurchaseOrders.Receives;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Souccar.SaleManagement.Receivings.Services
 {
     public class ReceivingDomainService : SouccarDomainService<Receiving, int>, IReceivingDomainService
     {
         private readonly IRepository<Receiving, int> _receivingRepository;
-        public ReceivingDomainService(IRepository<Receiving, int> receivingRepository) : base(receivingRepository)
+        private readonly IRepository<ReceivingItem, int> _receivingItemRepository;
+        public ReceivingDomainService(IRepository<Receiving, int> receivingRepository, IRepository<ReceivingItem, int> receivingItemRepository = null) : base(receivingRepository)
         {
             _receivingRepository = receivingRepository;
+            _receivingItemRepository = receivingItemRepository;
         }
 
         public IQueryable<Receiving> GetAllByInvoiceId(int invoiceId)
@@ -21,6 +26,17 @@ namespace Souccar.SaleManagement.Receivings.Services
                 .Include(i => i.ReceivingItems).ThenInclude(x => x.InvoiceItem).ThenInclude(x => x.OfferItem).ThenInclude(x => x.Unit)
                 .Include(i => i.ReceivingItems).ThenInclude(x => x.InvoiceItem).ThenInclude(x => x.OfferItem).ThenInclude(x => x.Size)
                 .Where(x => x.InvoiceId == invoiceId);
+        }
+
+        public IList<ReceivingItem> GetItemsByReceivingId(int receivingId)
+        {
+            var items = _receivingItemRepository.GetAll().Where(x => x.ReceivingId == receivingId).ToList();
+            return items;
+        }
+
+        public async Task DeleteItemAsync(int itemId)
+        {
+            await _receivingItemRepository.DeleteAsync(itemId);
         }
     }
 }
